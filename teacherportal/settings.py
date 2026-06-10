8116c946a60f16666bb32916699c45a1938e7be7
Raja <info.rajataimoorbagh@gmail.com>
2026-06-10 09:04:11 +0500

    Fix Railway deployment

--- teacherportal/settings.py
+++ teacherportal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-np8k0fe%v7fp)c1+iqmiy4(ovllzk8tsm*h@_+uh&jxm@!&f57
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
